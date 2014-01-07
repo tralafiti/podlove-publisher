ab31546138b85c13dfa11fffc51c12432137d6d9
chemiker <alex@Skylake.local>
2014-01-07 23:05:51 +0100

    Added Episode Numbering to Episode Form

--- lib/modules/episode_numbering/episode_numbering.php
+++ lib/modules/episode_numbering/episode_numbering.php
@@ -15,6 +15,8 @@ class Episode_Numbering extends \Podlove\Modules\Base {
     {
     	add_action( 'podlove_module_was_activated_episode_numbering', array( $this, 'was_activated' ) );
 
+        add_action( 'save_post', array( $this, 'save_post_meta' ) );
+
     	add_action( 'podlove_podcast_form', array( $this, 'podcast_form_extension' ), 10, 2 );
     	add_action( 'podlove_episode_form', array( $this, 'episode_form_extension' ), 10, 2 );
 
@@ -26,6 +28,13 @@ class Episode_Numbering extends \Podlove\Modules\Base {
     	// Add mnemonic property to podcast model
     	$podcast = \Podlove\Model\Podcast::get_instance();
     	$podcast->property( 'mnemonic' );
+    }
+
+    public function save_post_meta( $post_id )
+    {
+        update_post_meta( $post_id, '_podlove_meta_podlove_episode_season', $_POST['_podlove_meta']['_podlove_episode_season'] );
+        update_post_meta( $post_id, '_podlove_meta_podlove_episode_number', $_POST['_podlove_meta']['_podlove_episode_number'] );
+        update_post_meta( $post_id, '_podlove_meta_podlove_episode_global_number', $_POST['_podlove_meta']['_podlove_episode_global_number'] );
     }
 
     public function was_activated( $module_name )
@@ -40,11 +49,53 @@ class Episode_Numbering extends \Podlove\Modules\Base {
 
     public function episode_form_extension( $wrapper )
     {
-    	$wrapper->callback( 'podlove_episode_numbering', array(
+    	$wrapper->callback( '_podlove_episode_season', array(
     		'label'    => __( 'Numbering', 'podlove' ),
-    		'callback' => function() {
-    			echo "fuu";
-    		}
+    		'description' => __( 'Used to call episodes by their global episode number.' ),
+            'callback'  => function() {
+
+                $seasons = array_reverse( \Podlove\Modules\EpisodeNumbering\Model\Season::all() );
+
+                $post_id = get_the_ID();
+
+                $season_id = get_post_meta( $post_id, '_podlove_meta_podlove_episode_season', true );
+                $episode_number = get_post_meta( $post_id, '_podlove_meta_podlove_episode_number', true );
+                $global_episode_number = get_post_meta( $post_id, '_podlove_meta_podlove_episode_global_number', true );
+
+                ?>
+                    <style type="text/css">
+                        .podlove-episode-numbering-item {
+                            width: 30% !important;
+                            display: inline-block;
+                            margin-right: 2%;
+                        }
+                    </style>
+
+                    <div class="podlove-episode-numbering-item">
+                        <label for="_podlove_meta_podlove_episode_season">Season</label>
+                        <select name="_podlove_meta[_podlove_episode_season]" id="_podlove_meta_podlove_episode_season" class="chosen" >
+                            <?php
+                                foreach ( $seasons as $season_key => $season ) {
+                                   printf( '<option value="%s"%s>%s</option>',
+                                            $season->id,
+                                            $season_id == $season->id ? ' selected' : '',
+                                            $season->number ); 
+                                }
+                            ?>
+                        </select>
+                    </div>
+
+                    <div class="podlove-episode-numbering-item">
+                        <label for="_podlove_meta_podlove_episode_number">Episode Number</label>
+                        <input type="text" name="_podlove_meta[_podlove_episode_number]" id="_podlove_meta_podlove_episode_number" value="<?php echo $episode_number; ?>" >
+                    </div>
+
+                    <div class="podlove-episode-numbering-item">
+                        <label for="_podlove_meta_podlove_episode_global_number">Global Episode Number</label>
+                        <input type="text" name="_podlove_meta[_podlove_episode_global_number]" id="_podlove_meta_podlove_episode_global_number" value="<?php echo $global_episode_number; ?>" >
+                    </div>
+                <?php
+            }
     	) );
     }
 
