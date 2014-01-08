bec83508a867d4693bc96d9c121719e0fbe9e1da
chemiker <alex@nb-168-135.hirnport.ruhr-uni-bochum.de>
2014-01-08 16:00:52 +0100

    Added Episode number and Preview

--- lib/modules/episode_numbering/episode_numbering.php
+++ lib/modules/episode_numbering/episode_numbering.php
@@ -56,60 +56,167 @@ class Episode_Numbering extends \Podlove\Modules\Base {
 
                 $seasons = array_reverse( \Podlove\Modules\EpisodeNumbering\Model\Season::all() );
                 $episodes = \Podlove\Model\Episode::all();
+                $podcast = \Podlove\Model\Podcast::get_instance();
 
                 $post_id = get_the_ID();
-
-                // Unset all Episodes that are drafts
-                foreach ( $episodes as $episode_key => $episode ) {
-                    $episode_status = get_post( $episode->post_id )->post_status;
-                    if ( $episode_status == 'auto-draft' || 
-                         $episode_status == 'inherit' ||
-                         $episode_status == 'trash' )
-                        unset( $episodes[$episode_key] );
-                }
-
-                // The global episode number is the number of episodes + 1!
-                $number_of_episodes = count( $episodes ) + 1; 
 
                 $season_id = get_post_meta( $post_id, '_podlove_meta_podlove_episode_season', true );
                 $episode_number = get_post_meta( $post_id, '_podlove_meta_podlove_episode_number', true );
                 $global_episode_number = get_post_meta( $post_id, '_podlove_meta_podlove_episode_global_number', true );
 
+                $current_season_id = $seasons[0]->id;
+                $current_season_episode_counter = 0;
+
+                // Unset all Episodes that are drafts & fetch Episode Number
+                foreach ( $episodes as $episode_key => $episode ) {
+                    $episode_status = get_post( $episode->post_id )->post_status;
+                    if ( $episode_status == 'auto-draft' || 
+                         $episode_status == 'inherit' ||
+                         $episode_status == 'trash' )
+                    {
+                        unset( $episodes[$episode_key] );
+                    } else 
+                    {
+                        if ( $season_id == $current_season_id )
+                            $current_season_episode_counter++;
+                    }
+                }
+
+                // The global episode number is the number of episodes + 1!
+                $number_of_episodes = count( $episodes ) + 1; 
+
                 ?>
                     <style type="text/css">
                         .podlove-episode-numbering-item {
-                            width: 30% !important;
                             display: inline-block;
                             margin-right: 2%;
+                            line-height: 26px;
+                        }
+                        .podlove-global-number, .podlove-season-number, .podlove-season-mnemonic {
+                            font-family: Consolas;
+                            background-color: #E7E7E7;
+                            margin: 0px 2px 0px 2px;
+                        }
+                        #podlove-episode-numbering-preview {
+                            margin-top: 15px;
+                        }
+                        .podlove-episode-numbering-item input, .podlove-episode-numbering-item select {
+                            width: 50px;
+                            display: inline-block;
+                            height: 26px;
+                        }
+                        .podlove-episode-numbering-item label {
+                            display: inline-block;
+                            margin-right: 10px;
+                        }
+                        .podlove-episode-numbering-item.episode_number {
+                            width: 170px !important;
+                        }
+                        .podlove-episode-numbering-item.episode_season {
+                            width: 150px !important;
+                        }
+                        .podlove-episode-numbering-item.episode_global_number {
+                            width: 250px !important;
                         }
                     </style>
 
                     <script type="text/javascript">
+                        var PODLOVE = PODLOVE || {};
+                        var podcast_mnemonic = "<?php echo $podcast->mnemonic; ?>";
+                        var season_mnemonic = "";
+
+                        PODLOVE.fill_string_left = function( number, length, fill ) {
+                            number = number.toString();
+                            
+                            if ( !fill ) { fill = '0'; } // found @ http://stv.whtly.com/2009/02/27/simple-jquery-string-padding-function/
+                            
+                            while ( number.length < length ) {
+                                number = fill + number;
+                            }
+
+                            return number;
+                        }
+                        
+                        PODLOVE.episode_numbering_preview = function() {
+                            (function($) {
+
+                                $("select#_podlove_meta_podlove_episode_season option:selected").each(function() {
+                                    season_mnemonic = $(this).data('mnemonic');
+                                });
+
+                                var episode_global_number = $("#_podlove_meta_podlove_episode_global_number").val();
+                                var episode_number = $("#_podlove_meta_podlove_episode_number").val();
+                                var episode_season = $("#_podlove_meta_podlove_episode_season").val();
+
+                                if( episode_global_number !== '' ) {
+                                    $(".podlove-global-number").html( podcast_mnemonic + PODLOVE.fill_string_left( episode_global_number, 3 ) );
+                                } else {
+                                    $(".podlove-global-number").html('');
+                                }
+
+                                if( episode_number !== '' ) {
+                                    $(".podlove-season-number").html( "S" + PODLOVE.fill_string_left( episode_season, 2 ) + "E" + PODLOVE.fill_string_left( episode_number, 2 ) );
+                                } else {
+                                     $(".podlove-season-number").html('');
+                                }
+
+                                if( episode_number !== '' ) {
+                                    $(".podlove-season-mnemonic").html( season_mnemonic + PODLOVE.fill_string_left( episode_number, 2 ) );
+                                 } else {
+                                    $(".podlove-season-mnemonic").html('');
+                                }
+
+                            }(jQuery));
+                        }
+
+                        jQuery(document).ready(function() {
+                            PODLOVE.episode_numbering_preview();
+
+                            jQuery("#_podlove_meta_podlove_episode_season").change(function() {
+                                 PODLOVE.episode_numbering_preview();
+                            });
+
+                            jQuery("#_podlove_meta_podlove_episode_number").change(function() {
+                                 PODLOVE.episode_numbering_preview();
+                            });
+
+                            jQuery("#_podlove_meta_podlove_episode_global_number").change(function() {
+                                 PODLOVE.episode_numbering_preview();
+                            });
+                        });
 
                     </script>
 
                     <div class="podlove-episode-numbering-item">
-                        <label for="_podlove_meta_podlove_episode_season">Season</label>
+                        <label for="_podlove_meta_podlove_episode_season episode_season">Season</label>
                         <select name="_podlove_meta[_podlove_episode_season]" id="_podlove_meta_podlove_episode_season" class="chosen" >
                             <?php
                                 foreach ( $seasons as $season_key => $season ) {
-                                   printf( '<option value="%s"%s>%s</option>',
+                                   printf( '<option value="%s"%s data-mnemonic="%s">%s</option>',
                                             $season->id,
                                             $season_id == $season->id ? ' selected' : '',
+                                            $season->mnemonic,
                                             $season->number ); 
                                 }
                             ?>
                         </select>
                     </div>
 
-                    <div class="podlove-episode-numbering-item">
+                    <div class="podlove-episode-numbering-item episode_number">
                         <label for="_podlove_meta_podlove_episode_number">Episode Number</label>
-                        <input type="text" name="_podlove_meta[_podlove_episode_number]" id="_podlove_meta_podlove_episode_number" value="<?php echo $episode_number; ?>" >
+                        <input type="number" min="1" step="1" pattern="\d+" name="_podlove_meta[_podlove_episode_number]" id="_podlove_meta_podlove_episode_number" value="<?php echo ( $episode_number == '' ? $current_season_episode_counter + 1 : $episode_number ); ?>" >
                     </div>
 
-                    <div class="podlove-episode-numbering-item">
+                    <div class="podlove-episode-numbering-item episode_global_number">
                         <label for="_podlove_meta_podlove_episode_global_number">Global Episode Number</label>
-                        <input type="text" name="_podlove_meta[_podlove_episode_global_number]" id="_podlove_meta_podlove_episode_global_number" value="<?php echo ( $global_episode_number == '' ? $number_of_episodes : $global_episode_number ); ?>" >
+                        <input type="number" min="1" step="1" pattern="\d+" name="_podlove_meta[_podlove_episode_global_number]" id="_podlove_meta_podlove_episode_global_number" value="<?php echo ( $global_episode_number == '' ? $number_of_episodes : $global_episode_number ); ?>" >
+                    </div>
+
+                    <div id="podlove-episode-numbering-preview">
+                        This Episode will be numbered as:
+                        <span class="podlove-global-number"></span>
+                        <span class="podlove-season-number"></span>
+                        <span class="podlove-season-mnemonic"></span>
                     </div>
                 <?php
             }
