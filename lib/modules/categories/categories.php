d0f3cedb8dd96b5878d4a51d7a7c2873959fb704
Chemiker <alexlueken@gmail.com>
2014-05-05 10:50:59 +0200

    Add custom feed title, subtitle, summary to cats

--- lib/modules/categories/categories.php
+++ lib/modules/categories/categories.php
@@ -13,6 +13,62 @@ class Categories extends \Podlove\Modules\Base {
 			$args['taxonomies'][] = 'category';
 			return $args;		
 		} );
+
+		add_action( 'category_edit_form_fields', array( $this, 'category_edit_form_fields' ) );
+		add_action( 'edited_category', array( $this, 'save_category_extra_fields' ) );
+
+		add_filter( 'the_category_rss', array( $this, 'feed_modification' ) );
+	}
+
+	public function feed_modification( $bloginfo_rss ) {
+		print_r($bloginfo_rss);
+		return $bloginfo_rss;
+	}
+
+	public function category_edit_form_fields() {
+		$podlove_feed_extension = get_option( 'podlove_category_extension_' . $_REQUEST['tag_ID'] );
+		?>
+		<tr class="form-field">
+			<th valign="top" scope="row">
+				<label for="catpic"><?php _e( 'Category Feed Title', 'podlove' ); ?></label>
+			</th>
+			<td>
+				<input type="text" id="podlove_feed_title" name="podlove[feed_title]" class="regular-text required" 
+				 value="<?php echo ( isset( $podlove_feed_extension['feed_title'] ) ? $podlove_feed_extension['feed_title'] : '' ); ?>" />
+			</td>
+		</tr>
+		<tr class="form-field">
+			<th valign="top" scope="row">
+				<label for="catpic"><?php _e( 'Category Feed Subtitle', 'podlove' ); ?></label>
+			</th>
+			<td>
+				<input type="text" id="podlove_feed_subtitle" name="podlove[feed_subtitle]" class="regular-text required"
+				 value="<?php echo ( isset( $podlove_feed_extension['feed_subtitle'] ) ? $podlove_feed_extension['feed_subtitle'] : '' ); ?>" />
+			</td>
+		</tr>
+		<tr class="form-field">
+			<th valign="top" scope="row">
+				<label for="catpic"><?php _e( 'Category Feed Summary', 'podlove' ); ?></label>
+			</th>
+			<td>
+				<textarea class="autogrow" cols="40" rows="3" name="podlove[feed_summary]"><?php echo ( isset( $podlove_feed_extension['feed_summary'] ) ? $podlove_feed_extension['feed_summary'] : '' ); ?></textarea>
+			</td>
+		</tr>
+		<?php
+	}
+
+	public function save_category_extra_fields( $category_id ) {
+		if ( isset( $_POST['podlove'] ) ) {
+			$c = $category_id;
+			$podlove_feed_extension = get_option( "podlove_category_extension_$c" );
+			$podlove_feed_keys = array_keys( $_POST['podlove'] );
+				foreach ( $podlove_feed_keys as $key ) {
+		 			if ( isset($_POST['podlove'][$key]) ) {
+						$podlove_feed_extension[$key] = $_POST['podlove'][$key];
+		 			}
+		   		}
+		       update_option( "podlove_category_extension_$c", $podlove_feed_extension );
+		   }
 	}
 
 }
