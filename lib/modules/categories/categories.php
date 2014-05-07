8673ae68ce8256f91d01672305cf8a740675b970
Chemiker <alexlueken@gmail.com>
2014-05-07 13:27:52 +0200

    Overwrite Title, Description, Subtitle & Summary

--- lib/modules/categories/categories.php
+++ lib/modules/categories/categories.php
@@ -17,21 +17,72 @@ class Categories extends \Podlove\Modules\Base {
 		add_action( 'category_edit_form_fields', array( $this, 'category_edit_form_fields' ) );
 		add_action( 'edited_category', array( $this, 'save_category_extra_fields' ) );
 
+		/** Check if a category is present. If so, check if the holy
+		 *  trinity of Title, Subtitle and Summary needs to be overwritten.
+		 */
+		add_filter( 'podlove_rss_feed_description', function( $feed_description ) {
+			$overwrite_options = Categories::get_overwrite_options();
+
+			if ( empty( $overwrite_options['description'] ) )
+				return $feed_description;
+
+			return $overwrite_options['description'];
+		});
 		add_filter( 'podlove_manipulate_feed_title', function( $feed_title ) {
-			return "asd";
-		});
+			$overwrite_options = Categories::get_overwrite_options();
 
+			if ( empty( $overwrite_options['feed_title'] ) )
+				return $feed_title;
+
+			return $overwrite_options['feed_title'];
+		});
 		add_filter( 'podlove_manipulate_feed_description', function( $feed_summary ) {
-			return "ololol";
-		});
+			$overwrite_options = Categories::get_overwrite_options();
 
-		add_filter( 'podlove_feed_itunes_subtitle', function( $foo ) {
-			return "<itunes:subtitle>FOOO</itunes:subtitle>";
-		});
+			if ( empty( $overwrite_options['feed_summary'] ) )
+				return $feed_summary;
 
-		add_filter( 'podlove_feed_itunes_summary', function( $foo ) {
-			return "<itunes:summary>FOOO</itunes:summary>";
+			return $overwrite_options['feed_summary'];
 		});
+		add_filter( 'podlove_feed_itunes_subtitle', function( $feed_subtitle ) {
+			$overwrite_options = Categories::get_overwrite_options();
+
+			if ( empty( $overwrite_options['feed_subtitle'] ) )
+				return $feed_subtitle;
+
+			return "<itunes:subtitle>" . $overwrite_options['feed_subtitle'] . "</itunes:subtitle>";
+		});
+		add_filter( 'podlove_feed_itunes_summary', function( $feed_summary ) {
+			$overwrite_options = Categories::get_overwrite_options();
+
+			if ( empty( $overwrite_options['feed_summary'] ) )
+				return $feed_summary;
+
+			return "<itunes:summary>" . $overwrite_options['feed_summary'] . "</itunes:summary>";
+		});
+	}
+
+	public static function get_overwrite_options() {
+		$category = Categories::get_category();
+
+		if ( is_null( $category ) )
+			return NULL;
+
+		$overwrite_options = get_option( 'podlove_category_extension_' . $category->term_id );
+		$overwrite_options['description'] = $category->description;
+
+		return $overwrite_options;
+	}
+
+	public static function get_category() {
+		global $wp_query;
+
+		if ( !get_query_var('category_name') )
+			return NULL;
+
+		$category = get_category_by_slug( get_query_var('category_name') );
+
+		return $category;
 	}
 
 	public function category_edit_form_fields() {
